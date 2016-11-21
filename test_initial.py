4d852081801fe6aa9f7f98551d115403362cabfe
Bogdan Radulescu <bogdan@nimblex.net>
2016-11-21 21:14:11 +0000

    Made some improvements to the test so they would be compatible with Python 3.5

--- test_initial.py
+++ test_initial.py
@@ -9,22 +9,20 @@ class MainTest(unittest.TestCase):
 
   def test_most_basic_route(self):
     rv = self.app.get('/testing')
-    assert 'This is a testing page' in rv.data
+    assert b'This is a testing page' in rv.data
 
   def test_get_firmware_web(self):
     rv = self.app.get('/firmware')
-    assert 'Complete below' in rv.data
+    assert b'Complete below' in rv.data
 
   # This test checks if the app retuns our new firmware correctly
   # Currently the md5 checksum is not of the file itself but of the complete response
   def test_get_firmware_esp_new(self):
     rv = self.app.get('/firmware',
                        environ_base={'HTTP_USER_AGENT': 'ESP8266-http-Update', 'HTTP_X_ESP8266_STA_MAC': '00:11:22:33:44:55', 'HTTP_X_ESP8266_VERSION': 'different'})
-#   print rv.response.__dict__
     hs = hashlib.md5()
-    for i in rv.response:
-      hs.update(i)
-    self.assertEqual(hs.hexdigest(), 'd41d8cd98f00b204e9800998ecf8427e')
+    hs.update(rv.data)
+    self.assertEqual(hs.hexdigest(), '80c9574fc2d169fe9c097239c2ed0b02')
 
   # This test checks the app when we don't have a new firmware
   def test_get_firmware_esp_current(self):
